c29bbcd4c02d35c47ab772bb4e811dac8bb3a434
shockwave-ron <shockwave.ron@gmail.com>
2026-06-15 18:54:33 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
-{
-  "buildCommand": "npm run build",
-  "outputDirectory": "dist",
-  "framework": "vite",
-  "rewrites": [
-    { "source": "/api/(.*)", "destination": "/api/$1" }
-  ]
-}
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+})
